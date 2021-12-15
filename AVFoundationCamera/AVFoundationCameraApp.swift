884545cfccafc1df618d1832bf53c1a4008abcc8
nortyvino <46572988+nortyvino@users.noreply.github.com>
2021-12-15 08:38:17 -0600

    Initial Commit

---
+++ AVFoundationCamera/AVFoundationCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVFoundationCameraApp.swift
+//  AVFoundationCamera
+//
+//  Created by Nick Boyer on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AVFoundationCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
